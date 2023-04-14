0e5f5d5d6d9a0cb964d71a1b88b5925c9f40a66f
Thomas Baudry <2132465@cegeprdl.ca>
2023-04-13 22:05:24 -0400

    Premier Commit
    Interface graphique faites.

---
+++ app/src/main/java/ca/cegeph2023/laporte_alarme/MainActivity.kt
@@ -0,0 +1,11 @@
+package ca.cegeph2023.laporte_alarme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
